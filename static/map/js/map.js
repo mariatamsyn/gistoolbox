6993b9900ec1895fd24e2fc9fb107843b26f77e2
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-10 20:57:12 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -28,5 +28,5 @@ function toggleFullscreen() {
 
 // coordinates
 map.on('mousemove', function(e) {
-    $('#cont-ft').html(`Lat: ${Math.round(e.latlng.lat,5)} Long: ${e.latlng.lng}`)
+    $('#cont-ft').html(`Lat: ${toString(Math.round(e.latlng.lat,5))} Long: ${e.latlng.lng}`)
 })
