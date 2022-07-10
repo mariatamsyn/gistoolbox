0417cd04785374dd672e5cfe8001bed47bc9cd9b
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-10 18:16:52 +0800

    misc

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -2,5 +2,6 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
