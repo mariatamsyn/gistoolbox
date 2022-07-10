b1c9d3cfb7927626baca5e0140ef8d462e8bd43e
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-10 19:31:15 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -1,11 +1,12 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
-map.zoomControl.setPosition('topright')
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// map zoom
+map.zoomControl.setPosition('topright')
 
 // marker
 L.marker([51.505, -0.09]).addTo(map)
