b417d3b5d61e4975ef83f8855c50aa45c049256d
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-10 18:26:14 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
+map.zoomControl.setPosition('topright')
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
