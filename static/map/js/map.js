a0570d86b2c46b177bfd7e0c29d32f59f90c7bd6
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-10 18:19:12 +0800

    misc

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -5,3 +5,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+L.marker([51.505, -0.09]).addTo(map)
+    .bindPopup('This is a pop-up!')
+    .openPopup();
+
