0ed4b7e8171917816ba127010f4ff6e21e081361
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-18 00:03:38 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -21,7 +21,7 @@ L.control.scale({position:'bottomright'}).addTo(map)
 const browserControl = L.control.browserPrint({position: 'bottomright'}).addTo(map);
 
 // leaflet measure 
-const measureControl = L.Control.Measure({position: 'bottomright'}).addTo(map)
+const measureControl = L.control.measure({position: 'bottomright'}).addTo(map)
 
 // coordinates
 map.on('mousemove', function(e) {
