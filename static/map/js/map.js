2353389f1c6caf5b04da8f19a4a6abfeda249a73
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-18 04:10:06 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -1,13 +1,9 @@
 // map functions
 const map = L.map('map').setView([51.505, -0.09], 13);
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-var wmsLayer = L.tileLayer.wms('https://ows.emodnet-bathymetry.eu/wms', {
-    layers: 'Coastlines'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // map zoom
