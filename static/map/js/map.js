b3ca02cb36dd30545806e23cea94afb7d24baf7a
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-24 03:02:14 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -22,10 +22,9 @@ const browserControl = L.control.browserPrint({position: 'bottomright'}).addTo(m
 
 // coordinates
 map.on('mousemove', function(e) {
-    console.log('here')
     y = e.latlng.lat
     x = e.latlng.lng
-    $('#pointer-coordinates-values').value = (`${round_num(x,5)}, ${round_num(y,5)}`)
+    document.getElementById('pointer-coordinates-values').value = (`${round_num(x,5)}, ${round_num(y,5)}`)
 })
 
 // geocoder
