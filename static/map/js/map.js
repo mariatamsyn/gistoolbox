b95828d852e2b9b4fd4ec2b717bfa763f2b4019b
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-24 00:23:49 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -28,21 +28,8 @@ map.on('mousemove', function(e) {
 })
 
 // geocoder
-//L.Control.geocoder({position: 'bottomright'}).addTo(map);
-var geocoder = L.Control.geocoder({
-    defaultMarkGeocode: false
-  })
-    .on('markgeocode', function(e) {
-      var bbox = e.geocode.bbox;
-      var poly = L.polygon([
-        bbox.getSouthEast(),
-        bbox.getNorthEast(),
-        bbox.getNorthWest(),
-        bbox.getSouthWest()
-      ]).addTo(map);
-      map.fitBounds(poly.getBounds());
-    })
-    .addTo(map);
+L.Control.geocoder({position: 'bottomright'}).addTo(map);
+
 
 
 // fullscreen 
