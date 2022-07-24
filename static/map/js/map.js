84124db8b45bfcfd53875c83bf15c3f0e2313ee7
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-24 19:10:41 +0800

    leaflet

--- static/map/js/map.js
+++ static/map/js/map.js
@@ -6,40 +6,41 @@ function get_cursor_coords(e, precision) {
 }
 
 // leaflet map
-const map = L.map('map')
+const MAP = L.map('map')
 
 // basemaps
-const basemaps = {
+const BASEMAPS = {
     'OpenStreeMap' : L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
-    'Stadia Alidade Smooth Dark' : L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 20, attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'})
+    'Stadia Alidade Smooth Dark' : L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 20, attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'}),
+    'OpenTopoMap' : L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {maxZoom: 17,attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'})
 }
 
 // map defaults
-basemaps['OpenStreeMap'].addTo(map);
 let map_view = {coords:[51.505, -0.09], zoom:13}
-map.setView(map_view.coords,map_view.zoom);
+MAP.setView(map_view.coords,map_view.zoom);
+BASEMAPS['OpenStreeMap'].addTo(MAP);
 //let map_marker = L.marker(map_view.coords).addTo(map).bindPopup('This is a pop-up!').openPopup();
 
-// leaflet controls
+// LEAFLET CONTROLS
 
 //// zoom
-map.zoomControl.setPosition('topright')
+MAP.zoomControl.setPosition('topright')
 
 //// scale
-const map_scale = L.control.scale({position:'bottomright'}).addTo(map)
+const MAP_SCALE = L.control.scale({position:'bottomright'}).addTo(MAP)
 
 //// print
-const map_print = L.control.browserPrint({position: 'bottomright'}).addTo(map);
+const MAP_PRINT = L.control.browserPrint({position:'bottomright'}).addTo(MAP);
 
 //// cursor coordinates
-const cursor_coords_element = document.getElementById('pointer-coordinates-values')
-map.on('mousemove', function(e) {
+const CURSOR_COORDS_ELEMENT = document.getElementById('pointer-coordinates-values')
+MAP.on('mousemove', function(e) {
     cursor_coords = get_cursor_coords(e, 5)
-    cursor_coords_element.value = (`${cursor_coords.x}, ${cursor_coords.y}`)
+    CURSOR_COORDS_ELEMENT.value = (`${cursor_coords.x}, ${cursor_coords.y}`)
 })
 
 //// geocoder
-const map_geocoder = L.Control.geocoder({position: 'bottomright'}).addTo(map);
+const MAP_GEOCODER = L.Control.geocoder({position:'bottomright'}).addTo(MAP);
 
 
 
