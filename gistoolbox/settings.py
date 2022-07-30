5aaa8900c6b00933066d5fa11e9335d43dc4b0dd
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-31 00:07:26 +0800

    yaml

--- gistoolbox/settings.py
+++ gistoolbox/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-fcdxt_awdb7^d)$m*z6dw#3m(#n8r)4ve^$i@djk!t^&hh+jx5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
