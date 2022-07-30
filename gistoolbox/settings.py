2a53b0a7f9f58956531a5855c5d16f75211eb218
mariatamsyn <malabanan.mtc@gmail.com>
2022-07-30 23:47:06 +0800

    yaml

--- gistoolbox/settings.py
+++ gistoolbox/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from config import Config
 
 if os.name == 'nt':
     VENV_BASE = os.environ['VIRTUAL_ENV']
